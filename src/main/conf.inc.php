88f8c67b09b088652b488262d2f7643f4e121c85
jeremy <jeremy.zaire@hotmail.fr>
2015-04-29 19:38:06 +0200

    Delete conf.inc.php

--- src/main/conf.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-$bdd = new PDO('mysql:host=localhost;dbname=projetsi;charset=utf8', 'root', 'root');
-//$bdd = new PDO('mysql:host=mysql16.000webhost.com;dbname=a4724695_projet;charset=utf8', 'a4724695_zaire', 'L3MIAGEprojet');
-$bdd->query('SET NAMES UTF8');
-?>
